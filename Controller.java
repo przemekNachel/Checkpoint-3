8a90ff00c78e7f1053391bf1dd081f5c229ae399
przemekNachel <przemeknachel@gmail.com>
2018-02-19 09:51:23 +0100

    Add main menu

---
+++ Controller.java
@@ -0,0 +1,70 @@
+public class Controller {
+
+    View view = new View();
+    DAO dao = new DAO();
+
+    private static final String ADD_BOOK = "1";
+    private static final String EDIT_BOOK = "2";
+    private static final String DELETE_BOOK = "3";
+    private static final String SEARCH = "4";
+    private static final String SEE_BOOKS = "5";
+    private static final String SEE_BOOKS_BY_AUTHOR = "6";
+    private static final String EXIT = "0";
+
+    public void start() {
+
+        boolean terminateProgram = false;
+        while (!terminateProgram) {
+            view.showMainMenu();
+            String input = view.getUserInput();
+            switch (input) {
+                case ADD_BOOK:
+                    addBook();
+                    break;
+                case EDIT_BOOK:
+                    editBook();
+                    break;
+                case DELETE_BOOK:
+                    deleteBook();
+                    break;
+                case SEARCH:
+                    search();
+                    break;
+                case SEE_BOOKS:
+                    seeBooks();
+                    break;
+                case SEE_BOOKS_BY_AUTHOR:
+                    seeBooksByAuthor();
+                    break;
+                case EXIT:
+                    terminateProgram = true;
+                    break;
+                default:
+            }
+        }
+    }
+
+    private void addBook() {
+
+    }
+
+    private void editBook() {
+
+    }
+
+    private void deleteBook() {
+
+    }
+
+    private void search() {
+
+    }
+
+    private void seeBooks() {
+
+    }
+
+    private void seeBooksByAuthor() {
+
+    }
+}
---
+++ View.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class View {
+
+    public String getUserInput() {
+        Scanner input = new Scanner(System.in);
+        return input.nextLine();
+    }
+
+    public void clearScreen() {
+        System.out.print("\033\143");
+    }
+
+    public void showMainMenu() {
+        clearScreen();
+        System.out.println("JERZY'S HUMBLE HOME LIBRARY\n\n1. Add book\n2. Edit book\n3. Delete book");
+        System.out.println("4. Search\n5. See all books\n6. See books by author\n\n0. Exit");
+    }
+}
